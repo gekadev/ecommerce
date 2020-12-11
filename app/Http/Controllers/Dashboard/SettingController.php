f2e73b34ba1b95ad734f89d2dbc9ecad97d2a7eb
unknown <agaber.osman@gmail.com>
2020-12-11 13:28:23 +0200

    end shipping methods

--- app/Http/Controllers/Dashboard/SettingController.php
+++ app/Http/Controllers/Dashboard/SettingController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
+use App\Http\Requests\ShippingRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class SettingController extends Controller
 {
@@ -24,13 +27,33 @@ class SettingController extends Controller
             $dataView['shippingMethode'] = Setting::where(['key' =>'free_shipping_label'])->first();
         }
 
-      //return view('dashboard.settings.shipping.editShippingSettings')->with($dataView);
+      return view('dashboard.settings.shipping.editShippingSettings')->with($dataView);
 
 
     }
 
-    public  function updateShippingMethods()
+    public  function updateShippingMethods( ShippingRequest  $request, $id)
     {
+        try {
+            // get data
+            $dataView['shippingMethode'] = Setting::where(['id' =>$id])->firstOrfail();
+            //startpreparedata o be updated
+            DB::beginTransaction();
+            $shippingData = array('plain_value'	=>$request->plain_value);
+            //start update data
+            $updatedata = Setting::where(['id'=>$id])->update($shippingData);
+            $dataView['shippingMethode'] ->translate('en')->value = $request->value_en ;
+            $dataView['shippingMethode'] ->translate('ar')->value = $request->value_ar ;
+            $dataView['shippingMethode'] ->save();
+            DB::commit();
+            return redirect()->back()->with('success','تم تحديث البيانات بنجاح');
+        }catch ( \Exception $ex){
+            return redirect()->back()->with('error','خطاء في تحديث البيانات برجاء المحاوله مره اخري');
+            DB::rollBack();
+        }
+
+
+
 
     }
 }
